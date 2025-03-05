68620cd1534a3cdf2647894ef523ca60007d6758
fray osorio <frayosorio@gmail.com>
2025-03-05 12:12:26 -0500

    Se crear la interfaz grafica

---
+++ src/FrmJuego.java
@@ -0,0 +1,71 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.WindowConstants;
+
+public class FrmJuego extends JFrame {
+
+    private JButton btnRepartir;
+    private JButton btnVerificar;
+    private JPanel pnlJugador1;
+    private JPanel pnlJugador2;
+    private JTabbedPane tpJugadores;
+
+    public FrmJuego() {
+        btnRepartir = new JButton();
+        btnVerificar = new JButton();
+        tpJugadores = new JTabbedPane();
+        pnlJugador1 = new JPanel();
+        pnlJugador2 = new JPanel();
+
+        setSize(600, 300);
+        setTitle("Juego de Cartas");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        pnlJugador1.setBackground(new Color(153, 255, 51));
+        pnlJugador1.setLayout(null);
+        pnlJugador2.setBackground(new Color(0, 255, 255));
+        pnlJugador2.setLayout(null);
+
+        tpJugadores.setBounds(10, 40, 550, 170);
+        tpJugadores.addTab("Martín Estrada Contreras", pnlJugador1);
+        tpJugadores.addTab("Raul Vidal", pnlJugador2);
+
+        btnRepartir.setBounds(10, 10, 100, 25);
+        btnRepartir.setText("Repartir");
+        btnRepartir.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                btnRepartirClick(evt);
+            }
+        });
+
+        btnVerificar.setBounds(120, 10, 100, 25);
+        btnVerificar.setText("Verificar");
+        btnVerificar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                btnVerificarClick(evt);
+            }
+        });
+
+        getContentPane().setLayout(null);
+        getContentPane().add(tpJugadores);
+        getContentPane().add(btnRepartir);
+        getContentPane().add(btnVerificar);
+    }
+
+
+    private void btnRepartirClick(ActionEvent evt) {
+
+    }
+
+    private void btnVerificarClick(ActionEvent evt) {
+
+    }
+
+}
